3c4326e3a51da2a6ab9bdf72dfc1327dc1b07375
Shashank Phiske <111897076+Shashankphiske@users.noreply.github.com>
2023-08-27 21:58:57 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+class Pro{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello"); // use println instead of print to add a endline at the end of the string
+        System.out.println("\"Hello\""); // use "\"  \"" to add double quotes to your string
+                                           // to display \ use \\ 
+
+    }
+}
